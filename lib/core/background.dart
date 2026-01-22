8c2fb3954bda4619792d6cfd6ffddf8f37328c89
logan-music <nasrylogan@gmail.com>
2026-01-23 02:17:02 +0300

    Add files via upload

---
+++ lib/core/background.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'core/device_agent.dart';
+
+@pragma('vm:entry-point')
+Future<void> backgroundMain() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DeviceAgent.instance.startFromBackground();
+}
