ecd44322213b00f4711870fe8ee84258f58d6672
logan-music <nasrylogan@gmail.com>
2026-01-23 11:19:21 +0300

    Delete lib/core/background.dart

--- lib/core/background.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'core/device_agent.dart';
-
-@pragma('vm:entry-point')
-Future<void> backgroundMain() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await DeviceAgent.instance.startFromBackground();
-}
